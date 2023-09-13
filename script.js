0f318d97c752fbc3f7a5d8ee7dceca12fa49fe90
bobur <boburmakhmadkulov05@gmail.com>
2023-09-13 23:42:54 +0500

    hometask

---
+++ script.js
@@ -0,0 +1,20 @@
+let first_answer = prompt('What`s your name?')
+if (first_answer.charAt(0) === "a") {
+    console.log('Wellcome!');
+} else {
+    console.log('Get a hell!');
+}
+
+let second_answer = prompt('Fill the gap with random numbers!')
+if (second_answer % 2 === 0) {
+    console.log('четное число');
+} else {
+    console.log('нечетное число');
+}
+
+let third_answer = prompt('What`s your name')
+if (third_answer.length % 2 === 0) {
+    console.log('четное число')
+} else {
+    console.log('нечетное число');
+}
